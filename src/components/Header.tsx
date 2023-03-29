1da7480eaa52ad883ee84e2ec0a51568824ddb61
ElizavetaSavchenko <savchenko.liza.666@gmail.com>
2023-03-29 23:53:47 +0600

    feature-#861mhn9et/Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import { Colors } from '@styles/colors';
+
+export interface IHeader {
+    onMenuClick: () => void;
+    onCalendarClick: () => void;
+}
+
+export const Header: React.FC<IHeader> = props => {
+    const { onMenuClick, onCalendarClick } = props;
+    return (
+        <View style={styles.wrapper}>
+            <Pressable onPress={onMenuClick}>
+                <Image source={require('@assets/icons/menu-icon.png')} style={styles.img} />
+            </Pressable>
+            <Pressable onPress={onCalendarClick}>
+                <Image source={require('@assets/icons/calendar-icon.png')} style={styles.img} />
+            </Pressable>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    wrapper: {
+        width: '100%',
+        height: '100%',
+        backgroundColor: Colors.WHITE,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+    },
+
+    img: {
+        width: 48,
+        height: 48,
+    },
+});
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,3 +1,3 @@
 export const Colors = {
-    WHITE: '#ffffff',
+    WHITE: '#FFFFFF',
 };
