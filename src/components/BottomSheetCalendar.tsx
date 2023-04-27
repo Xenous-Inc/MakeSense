0e573de0e9b2aeb1d81b497033b2e1a1818b0fd2
DeathSwear <vlad0septa@yandex.ru>
2023-04-27 19:19:49 +0600

    [Test] Undone calendar Task: #85zrxk0ke

--- src/components/BottomSheetCalendar.tsx
+++ src/components/BottomSheetCalendar.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useMemo, useRef } from 'react';
 import { Text, StyleSheet } from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Calendar } from 'react-native-calendars';
 
 export interface IHeader {
     indexPos: boolean;
@@ -18,13 +19,25 @@ export const BottomSheetCalendar: React.FC<IHeader> = props => {
         bottomSheetRef.current?.snapToIndex(1);
     };
     return (
-        <BottomSheet ref={bottomSheetRef} index={2} snapPoints={snapPoints} enableHandlePanningGesture={false}>
+        <BottomSheet
+            ref={bottomSheetRef}
+            index={2}
+            snapPoints={snapPoints}
+            enableHandlePanningGesture={true}
+            handleIndicatorStyle={{ height: 0 }}
+            handleStyle={{ height: 0, padding: 0 }}
+        >
+            <Calendar
+                theme={{
+                    'arrowColor': '#000',
+                    'textDayFontWeight': 'bold',
+                    'stylesheet.calendar.header': { dayTextAtIndex0: { color: 'red' } },
+                    'todayTextColor': '#fff',
+                    'todayBackgroundColor': '#000',
+                }}
+                style={{ borderTopRightRadius: 13, borderTopLeftRadius: 13 }}
+            />
             {props.indexPos ? <Text>1</Text> : <Text>0</Text>}
-            {props.indexPos
-                ? null
-                : () => {
-                      handleClosePress();
-                  }}
         </BottomSheet>
     );
 };
